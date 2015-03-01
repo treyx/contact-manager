3bbbefc258edebd4a1a2be4ec6520203332f556c
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-01 15:40:42 -0700

    modified phone number form

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -69,22 +69,26 @@ RSpec.describe PhoneNumbersController, type: :controller do
 
   describe "POST #create" do
     context "with valid params" do
-      it "creates a new PhoneNumber" do
-        expect {
+      let(:alice) { Person.create(first_name: 'Alice', last_name: 'Smith') }
+      let(:valid_attributes) { {number: '555-1234', person_id: alice.id} }
+        it "creates a new PhoneNumber" do
+          expect {
+            post :create, {:phone_number => valid_attributes}, valid_session
+          }.to change(PhoneNumber, :count).by(1)
+        end
+  
+        it "assigns a newly created phone_number as @phone_number" do
           post :create, {:phone_number => valid_attributes}, valid_session
-        }.to change(PhoneNumber, :count).by(1)
-      end
-
-      it "assigns a newly created phone_number as @phone_number" do
-        post :create, {:phone_number => valid_attributes}, valid_session
-        expect(assigns(:phone_number)).to be_a(PhoneNumber)
-        expect(assigns(:phone_number)).to be_persisted
-      end
-
-      it "redirects to the created phone_number" do
-        post :create, {:phone_number => valid_attributes}, valid_session
-        expect(response).to redirect_to(PhoneNumber.last)
-      end
+          expect(assigns(:phone_number)).to be_a(PhoneNumber)
+          expect(assigns(:phone_number)).to be_persisted
+        end
+  
+        it "redirects to the phone number's person" do
+          alice = Person.create(first_name: 'Alice', last_name: 'Smith')
+          valid_attributes = {number: '555-8888', person_id: alice.id}
+          post :create, {:phone_number => valid_attributes}, valid_session
+          expect(response).to redirect_to(alice)
+        end
     end
 
     context "with invalid params" do
