5937a6b9928de209605786b5642781f6e0b4a129
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-01 20:08:25 -0700

    db migration to change phone numbers for polymorphic relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302023355) do
+ActiveRecord::Schema.define(version: 20150302025318) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -35,9 +35,10 @@ ActiveRecord::Schema.define(version: 20150302023355) do
 
   create_table "phone_numbers", force: :cascade do |t|
     t.string   "number"
-    t.integer  "person_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "contact_id"
+    t.string   "contact_type"
   end
 
 end
