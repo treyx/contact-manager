147065054b3e875537ffad70c90a4bc851bac078
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-01 19:49:20 -0700

    basic company validation

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let(:company) { Company.new(name: "Yahoo") }
+
+    it 'is valid' do
+      expect(company).to be_valid
+    end
+
+    it 'is invalid without a name' do
+      company.name = nil
+      expect(company).to_not be_valid
+    end
 end
