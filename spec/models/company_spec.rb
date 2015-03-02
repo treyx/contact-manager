995a1a1846be6ca1eb3f6fec008b45f7064b97ae
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-01 22:32:35 -0700

    added assertion for company spec

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -16,4 +16,9 @@ RSpec.describe Company, type: :model do
       phone_number = company.phone_numbers.build(number: "333-4444")
       expect(phone_number.number).to eq('333-4444')
     end
+
+    it "responds with its email addresses after they're created" do
+      email_address = company.email_addresses.build(address: "example@example.com")
+      expect(company.email_addresses.map(&:address)).to eq(["example@example.com"])
+    end
 end
