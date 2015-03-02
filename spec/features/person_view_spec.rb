c53c0a7305f5da37abee31b499176b3a3ae69385
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-01 17:35:29 -0700

    added link to add email address

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -74,5 +74,9 @@ describe 'the person view', type: :feature do
         expect(page).to have_selector('li', text: 'example@gmail.com')
       end
     end
+
+    it 'has a link to add a new email address' do
+      expect(page).to have_link('Add email address', href: new_email_address_path(person_id: person.id))
+    end
   end
 end
