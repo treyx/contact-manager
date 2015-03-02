a269bd7f92097af566a576cc0f8f0aa08a67104c
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-01 22:33:19 -0700

    modified email address spec

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
+  let(:company) { Company.create(name: "Yahoo") }
+
   let(:email_address) do
-    EmailAddress.new(address: 'example@example.com', person_id: '1')
+    EmailAddress.new(address: 'example@example.com', contact_id: 'company.id', contact_type: 'Company')
   end
 
   it 'is valid' do
@@ -14,8 +16,14 @@ RSpec.describe EmailAddress, type: :model do
     expect(email_address).not_to be_valid
   end
 
-  it 'is invalid without a person id' do
-    email_address.person_id = nil
+  it 'must have a reference to a contact' do
+    email_address.contact_id = nil
     expect(email_address).not_to be_valid
   end
+
+  it 'is associated with a contact' do
+    expect(email_address).to respond_to(:contact)
+  end
+
+
 end
