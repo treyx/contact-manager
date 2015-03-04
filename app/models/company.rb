e85908e444dcb28df0bebb30a01501d7ababa502
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-03 19:59:11 -0700

    added to_s method in company and person model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,8 @@
 class Company < ActiveRecord::Base
   validates :name, presence: true
   include Contact
+
+  def to_s
+    "#{name}"
+  end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,9 @@
 class Person < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
   include Contact
+
+  def to_s
+    "#{last_name}, #{first_name}"
+  end
 end
 
