255949314d410a4850485b4c7f30a3ac07b27ffb
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-03 19:57:55 -0700

    modified phone numbers edit and new views

--- spec/views/phone_numbers/new.html.erb_spec.rb
+++ spec/views/phone_numbers/new.html.erb_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "phone_numbers/new", type: :view do
+  let(:person) { Person.create(first_name: "Bob", last_name: "Smith") }
+  let(:phone_number) { PhoneNumber.new(contact_id: person.id, contact_type: 'Person', number: "867-5309") }
+
   before(:each) do
-    assign(:phone_number, PhoneNumber.new(
-      :number => "MyString",
-      :contact_id => 1
-    ))
+    assign(:phone_number, phone_number)
   end
 
   it "renders new phone_number form" do
@@ -17,5 +17,10 @@ RSpec.describe "phone_numbers/new", type: :view do
 
       assert_select "input#phone_number_contact_id[name=?]", "phone_number[contact_id]"
     end
+  end
+
+  it "shows the contact's name in the title" do
+    render
+    assert_select("h1", text: "New Phone Number for #{phone_number.contact.last_name}, #{phone_number.contact.first_name}")
   end
 end
