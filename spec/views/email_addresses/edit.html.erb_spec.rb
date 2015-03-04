8c72eb96c577b68e3e646043fc6759b01aa749a0
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-03 19:56:40 -0700

    modified email address edit and new views

--- spec/views/email_addresses/edit.html.erb_spec.rb
+++ spec/views/email_addresses/edit.html.erb_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "email_addresses/edit", type: :view do
+  let(:person) { Person.create(first_name: "Bob", last_name: "Smith") }
+  let(:email_address) { EmailAddress.create(contact_id: person.id, contact_type: 'Person', address: "MyString") }
+  
   before(:each) do
-    @email_address = assign(:email_address, EmailAddress.create!(
-      :address => "MyString",
-      :contact_id => 1
-    ))
+    @email_address = assign(:email_address, email_address)
   end
 
   it "renders the edit email_address form" do
@@ -17,5 +17,10 @@ RSpec.describe "email_addresses/edit", type: :view do
 
       assert_select "input#email_address_contact_id[name=?]", "email_address[contact_id]"
     end
+  end
+
+  it "shows the contact's name in the title" do
+    render
+    assert_select("h1", text: "Editing Email Address for #{@email_address.contact.last_name}, #{@email_address.contact.first_name}")
   end
 end
